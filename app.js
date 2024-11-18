8f6ea2385a0786dc4f97717957adfd645fc8233d
Kunal Mali <kunalmali20251@gmail.com>
2024-11-18 18:15:44 +0530

    Prime Number Code

---
+++ app.js
@@ -0,0 +1,10 @@
+let  numbers = [19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30];
+
+let primeArray = [];
+ for (let i=0; i<numbers.length; i++){
+    // Sorry evaluation Team Masai 
+    // I forgot the logic of prime number code 
+    // i will revice that topic 
+    // for now I am submitting this file and push to git hub
+    
+ }
